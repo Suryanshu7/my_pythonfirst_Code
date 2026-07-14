55d3a5e735aae8bdd96b712bf5b59e21649df212
Suryanshu Negi <161579484+Suryanshu7@users.noreply.github.com>
2026-07-14 21:28:26 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,21 @@
+"""
+Problem Statement: Write a Python program that does the following:
+  1. Takes two numbers as input from the user.
+  2. Performs the basic mathematical operations on these two numbers:
+  Addition
+  Subtraction
+  Multiplication
+  Division
+  3. Displays the results of each operation on the screen.
+
+"""
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the Second number: "))
+addition = num_1 + num_2
+subtraction = num_1 - num_2
+multiplication = num_1 * num_2
+division = num_1 / num_2
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
